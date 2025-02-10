1aeb2898ea5d9a9e9ecb044aa7048ac11704e694
rsm-aburda <aburda@ucsd.edu>
2025-02-10 11:28:03 -0800

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,79 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted, computed, watch } from 'vue'
+
+const todos = ref([])
+const name = ref('')
+
+const input_content = ref('')
+const input_category = ref(null)
+
+const todos_asc = computed(() => todos.value.sort((a, b) => {
+  return b.createdAt - a.createdAt
+
+}))
+
+const addTodo = () => {}
+watch(name, (newVal) =>{
+  localStorage.setItem('name', newVal)
+})
+
+onMounted(() => {
+  name.value = localStorage.getItem('name') || ''
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <main class = "app">
+    <section class = "greeting">
+      <h2 class = "title">
+        What's up, <input type = "text" placeholder = "Name here" v-model = "name" />
+      </h2>
+    </section>
+    
+    <section class = 'create-todo'>
+      <h3>CREATE A TODO</h3>
+
+      <form @submit.prevent = "addTodo">
+        <h4>What's on your todo list?</h4>
+        <input 
+          type = "text" 
+          placeholder = "e.g. make a video" 
+          v-model = 'input_content'/>
+
+        <h4>Pick a category</h4>
+        <div class = "options">
+          <label>
+            <input 
+              type = "radio" 
+              name = "category" 
+              
+              value = 'business'
+              v-model = 'input_category'/>
+
+            <span class = 'bubble business'></span>
+            <div>Business</div>
+          </label>
+
+          <label>
+            <input 
+              type = "radio" 
+              name = "category" 
+              
+              value = 'Personal'
+              v-model = 'input_category'/>
+
+            <span class = 'bubble personal'></span>
+            <div>Personal</div>
+          </label>
+
+        </div>
+
+        
+
+      </form>      
+    </section>
+  </main>
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
